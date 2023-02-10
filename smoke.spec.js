3eb06de4289e988b4e4cd842fb8a2081f5ce5186
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-02-09 23:09:03 -0800

    --resolve merge conflicts

--- smoke.spec.js
+++ smoke.spec.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 describe('truth', () => {
   it('returns true', () => {
-=======
-describe('smoke', () => {
-  it('should return true', () => {
->>>>>>> main
     expect(true).toEqual(true);
   });
 })
